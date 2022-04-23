5156c0b4be3d2c227e1a9a3e76fb92a0f54d0600
Zheng Li(MarsOriginal) <zlii0145@student.monash.edu>
2022-04-23 11:48:41 +1000

    make it better

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.get('/aboutdiabetes', (req, res) => {
     res.render('about_diabetes.hbs')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log('App is listening on port 3000!')
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env)
 })
