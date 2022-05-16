5a71e766945dcabf116a1c30cac02a8e102ddb1c
VYaoYao <1367247905vv@gmail.com>
2022-05-16 13:08:43 +1000

    Complete the feature of turning page

---
+++ public/js/turnpages.js
@@ -0,0 +1,9 @@
+function nextpageAction(current_page) {
+    current_page += 1;
+    return current_page
+}
+
+function lastpageAction(current_page) {
+    current_page -= 1;
+    return current_page
+}
