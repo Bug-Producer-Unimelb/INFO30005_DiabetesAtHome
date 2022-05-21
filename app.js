453bbb8a5b633df09fe1488592cef32324c445cb
Zheng Li(MarsOriginal) <zlii0145@student.monash.edu>
2022-05-21 14:43:24 +1000

    Remove moment

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')
 const mongooseClient = require('./models')
 const mongoose = require('mongoose')
-const moment = require('moment')
 
 const Comment = require('./models/comment')
 
@@ -94,124 +93,6 @@ app.get('/', (req, res) => {
     res.render('index.hbs')
 })
 
-/**
- * Calculate the encoragement rate and return it
- * @param {*} record
- */
-const calculateEncoragement = (record) => {
-    let rate = 0.0
-    if (record) {
-        rate = parseFloat(
-            (
-                (record.current_record_quantity * 1.0) /
-                record.total_quantity
-            ).toFixed(3)
-        )
-    }
-    return rate
-}
-
-app.get('/patients/:id/achievement', async (req, res) => {
-    const patientId = req.params.id
-    // console.log("patient id is: ", patientId);
-    // const record = await Record.findOne({'patient_id': patientId});
-    // const currentUser = await Patient.findOne({ _id: record.patient_id }).populate("user_id");
-    // const currentUserName = currentUser.user_id.username;
-    const currentUser = 'sdsd'
-    console.log('current user is: ', currentUser)
-    // const records = await Record.find();
-    const records = []
-    // console.log("all records are: ", records);
-    let allRankInfo = []
-    // get all the encoragement rate of all the patients
-    if (records && records.length > 0) {
-        for (const r of records) {
-            allRankInfo.push({
-                patient_id: r.patient_id,
-                rate: calculateEncoragement(r),
-            })
-        }
-    }
-    // sort the allRankInfo array above
-    allRankInfo.sort((a, b) => {
-        return -1 * (a.rate - b.rate)
-    })
-    let patientIds = []
-    for (let i = 0; i < allRankInfo.length; ++i) {
-        patientIds.push(allRankInfo[i].patient_id)
-    }
-
-    const patients = await Patient.find({ _id: { $in: patientIds } }).limit(5)
-    // console.log("all patients are: ", patients);
-    let userIds = []
-    if (patients && patients.length > 0) {
-        for (const p of patients) {
-            // console.log(typeof p.user_id);
-            userIds.push(p.user_id._id)
-        }
-    }
-
-    // console.log("all user ids are: ", userIds);
-    const users = await User.find({ _id: { $in: userIds } })
-    // console.log("all user are: ", users);
-    let top5Users = []
-    if (users && users.length > 0) {
-        for (const user of users) {
-            top5Users.push({
-                username: user.username,
-            })
-        }
-    }
-    console.log('all user names are: ', top5Users)
-
-    // query all the patient information according to the patient id
-
-    const user = await User.findById(patientId)
-
-    console.log('user')
-    let joined = user.createdAt
-    let today = new Date()
-    let n = moment(today).diff(joined, 'days')
-
-    console.log(n)
-
-    const comments = await Comment.aggregate([
-        { $match: { patient_id: mongoose.Types.ObjectId(patientId) } },
-        {
-            $project: {
-                time: { $dateToString: { format: '%Y-%m-%d', date: '$date' } },
-            },
-        },
-        { $group: { _id: '$time', count: { $sum: 1 } } },
-    ])
-
-    console.log(comments)
-
-    // console.log(user.createdAt)
-
-    let rate = parseInt((comments.length / n) * 100)
-
-    // let rate = 89
-
-    let show = false
-
-    if (rate >= 80) {
-        show = true
-    } else {
-        show = false
-    }
-
-    console.log(rate)
-    // if (record) {
-    //     rate = calculateEncoragement(record);
-    // }
-    return res.render('achievement.hbs', {
-        currentUserName: 'currentUserName',
-        show: show,
-        rate: rate,
-        top5Users: top5Users,
-    })
-})
 
 app.get('/aboutus', (req, res) => {
     res.render('about_us.hbs')
